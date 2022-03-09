adc4ca628c375c29c134a145738a5c429f38f6b0
Lee Clark <lee.clark7878@gmail.com>
2022-03-09 10:39:30 +0000

    added authentication

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "crastarter": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
